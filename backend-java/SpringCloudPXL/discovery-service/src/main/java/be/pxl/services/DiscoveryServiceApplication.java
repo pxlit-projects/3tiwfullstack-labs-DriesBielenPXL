91e92c92d57cb09e269b820b8d6ec1ab436e0c5e
Dries Bielen <dries.bielen10@hotmail.be>
2025-10-07 21:09:11 +0200

    Lab 3

--- backend-java/SpringCloudPXL/discovery-service/src/main/java/be/pxl/services/DiscoveryServiceApplication.java
+++ backend-java/SpringCloudPXL/discovery-service/src/main/java/be/pxl/services/DiscoveryServiceApplication.java
@@ -2,12 +2,14 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Discovery Service
  *
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication
 {
     public static void main( String[] args )
--- backend-java/SpringCloudPXL/notification-service/src/main/java/be/pxl/services/NotificationServiceApplication.java
+++ backend-java/SpringCloudPXL/notification-service/src/main/java/be/pxl/services/NotificationServiceApplication.java
@@ -2,11 +2,13 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * EmployeeServiceApplication
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication
 {
     public static void main( String[] args )
